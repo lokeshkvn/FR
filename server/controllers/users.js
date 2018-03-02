a2a95563d6ed5332504f28502245691873c0fc5b
Swati Kanchan <swatikanchan707@gmail.com>
2018-03-02 12:09:05 +0530

    Updated and added more code for controller functions

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,7 +8,6 @@ module.exports = {
     getUsers: async (req, res, next)=>{
         const users = await User.find({});
         res.status(200).json(users);
-        res.json(users);
     },
 
     getFarmers: async (req, res, next)=>{
@@ -42,7 +41,6 @@ module.exports = {
         req.user = user;
         let newFarmer = new Farmer({
             name: req.body.user_name,
-            user_type: "2",
             user_id: req.user,
             email: req.body.email,
             phone: req.body.phone,
@@ -52,16 +50,15 @@ module.exports = {
             account_info_id: req.body.account_info_id,
             card_info_id: req.body.card_info_id
         });
-        var farmer = await newFarmer.save();
+        let farmer = await newFarmer.save();
         farmer = farmer.populate({path: "user_id", model: "User"}, (err) => {
             if(err){
                 console.log("Error while populating");
                 res.json({error: err});
             } else {
-                res.json({msg: "Farmer created successfully", farmer_details: farmer});
+                res.status(201).json({msg: "Farmer created successfully", farmer_details: farmer});
             }
         });
-        // res.json({msg: "Farmer created successfully", farmer_details: farmer});
     },
 
     getRetailers: async (req, res, next)=>{
@@ -99,11 +96,17 @@ module.exports = {
             email: req.body.email,
             phone: req.body.phone,
             address: req.body.address,
-            date_of_birth: req.body.date_of_birth,
-            card_info_id: req.body.card_info_id
+            date_of_birth: req.body.date_of_birth
         });
-        const retailer = await newRetailer.save();
-        res.json({msg: "retailer created successfully", retailer_details: retailer});
+        let retailer = await newRetailer.save();
+        retailer = retailer.populate({path: "user_id", model: "User"}, (err) => {
+            if(err){
+                console.log("Error while populating");
+                res.json({error: err});
+            } else {
+                res.status(201).json({msg: "retailer created successfully", retailer_details: retailer});
+            }
+        });
     },
 
     addCard: async (req, res, next)=>{
@@ -112,10 +115,23 @@ module.exports = {
         console.log(retailer);
         const card = new RetailerCardInfo(req.body);
         card.retailer_id = retailer;
-        const saved_card = await card.save();
+        let saved_card = await card.save();
+        saved_card = saved_card.populate({path: "retailer_id", model: "Retailer"}, (err) => {
+            if(err){
+                console.log("Error while populating the card");
+                res.json({err: err});
+            }
+        });
         retailer.card_info_id.push(saved_card);
         retailer = await retailer.save();
-        res.status(201).json({msg: "card added successfully", card_details: saved_card, retailer_details: retailer});
+        retailer = retailer.populate({path: "card_info_id", model: "RetailerCardInfo"}, (err) => {
+            if(err) {
+                console.log("Error populating retailer's carf info");
+                res.json({err: err});
+            } else {
+                res.status(201).json({msg: "card added successfully", card_details: saved_card, retailer_details: retailer});
+            }
+        })
     },
 
     removeCard: async (req, res, next)=>{
@@ -127,6 +143,33 @@ module.exports = {
         await card.save();
         retailer = await retailer.save();
         res.status(201).json({msg: "card removed successfully", retailer_details: retailer});
+    },
+
+    getCard: async (req, res, next) => {
+        const {retailer_id} = req.params;
+        let retailer = await Retailer.findById(retailer_id);
+        retailer = retailer.populate({path: "card_info_id", model: "RetailerCardInfo"}, (err) => {
+            if(err){
+                console.log("Error while populating card info");
+                res.json({err: err});
+            } else {
+                let cards = retailer.card_info_id;
+                res.status(201).json(cards);
+            }
+        });
+    },
+
+    getOneCard: async (req, res, next) => {
+        const {card_id} = req.params;
+        let card = await RetailerCardInfo.findById(card_id);
+        card = card.populate({path: "retailer_id", model: "Retailer"}, (err) => {
+            if(err){
+                console.log("Error while populating card info");
+                res.json({err: err});
+            } else {
+                res.status(201).json(card);
+            }
+        });
     },
 
     getAdministrators: async (req, res, next)=>{
@@ -158,8 +201,15 @@ module.exports = {
             date_of_birth: req.body.date_of_birth,
             admin_type: req.body.admin_type
         });
-        const administrator = await newAdministrator.save();
-        res.json({msg: "administrator created successfully", administrator_details: administrator});
+        let administrator = await newAdministrator.save();
+        administrator = administrator.populate({path: "user_id", model: "User"}, (err) => {
+            if(err){
+                console.log("Error while populating");
+                res.json({error: err});
+            } else {
+                res.status(201).json({msg: "administrator created successfully", administrator_details: administrator});
+            }
+        })
     },
 
     loginUser: async (req, res, next)=>{
@@ -167,9 +217,9 @@ module.exports = {
             user_name: req.user_name,
             password: req.password
         };
+        console.log(user.user_name);
         const found_user = await User.findOne({user_name: user.user_name});
         console.log("Found the user");
-        console.log(found_user);
         if(found_user.user_type == "1"){
             console.log("user_type: Administrator");
             const found_administrator = await Administrator.findById(found_user._id);
@@ -184,9 +234,6 @@ module.exports = {
             console.log("user_type: Retailer");
             const found_retailer = await Retailer.findOne(found_user._id);
             res.json(found_retailer)
-        }
-        else{
-            console.log("Not found")
         }
     }
 }
